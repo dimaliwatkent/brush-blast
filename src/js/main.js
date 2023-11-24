01f39c1c2b3c65bea0ff85a718537ff8c7105ffa
dimaliwatkent <dimaliwatkent@gmail.com>
2023-11-25 07:43:16 +0800

    added navbar

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,8 @@ import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
+
+// $(".nav-link").on("click", function () {
+//   $(".nav-link").removeClass("active");
+//   $(this).addClass("active");
+// });
