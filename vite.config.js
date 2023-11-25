a5092dbe7aa37c96cea851ac5c3b5448216258a2
dimaliwatkent <dimaliwatkent@gmail.com>
2023-11-25 11:54:20 +0800

    added gitpages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-
-const path = require('path')
+const path = require("path");
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  // root: path.resolve(__dirname, "src"),
   resolve: {
     alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+    },
   },
 
   server: {
     port: 8080,
-    hot: true
-  }
-}
+    hot: true,
+  },
+};
